a2376a107cecc68cf82d6a36c6673ceab18a9abb
qwertytoki <qwertytoki@gmail.com>
2024-07-27 22:32:50 +0800

    move menuItem.ts to service class

--- backend/src/routes/menuRoutes.ts
+++ backend/src/routes/menuRoutes.ts
@@ -1,30 +1,26 @@
 import { Router } from "express"
-import { getMenuItems } from "../menuItems";
 import { MenuDetail } from "../entitiies/MenuDetail";
+import { MenuService } from "../services/MenuService";
 
 const router = Router();
 
 router.get('/menu', (req, res) => {
-    const menuItems = getMenuItems();
+    const menuItems = MenuService.getMenuItems();
     res.json(menuItems);
 });
 
 router.get('/menu/:id', (req, res) => {
     const { id } = req.params;
-    const menuItems = getMenuItems();
     const parsedId = parseInt(id);
-    const menu = menuItems.find(item => item.id === (isNaN(parsedId) ? -1 : parsedId));
-    if (menu) {
-        const menuDetail = new MenuDetail(
-            menu.id, 
-            menu.name, 
-            menu.store_name, 
-            menu.review_score, 
-            menu.photo_url, 
-            [], []);
+    if(isNaN(parsedId)) {
+        res.status(400).send('Invalid id');
+        return;
+    }
+    const menuDetail = MenuService.getMenuDetail(parsedId);
+    if(menuDetail) {
         res.json(menuDetail);
     } else {
-        res.status(404).json({ message: "Menu not found" });
+        res.status(404).send('Menu not found');
     }
 });
 
---
+++ backend/src/services/MenuService.ts
@@ -0,0 +1,30 @@
+import { Menu } from "../entitiies/Menu";
+import { MenuDetail } from "../entitiies/MenuDetail";
+
+export class MenuService {
+    static getMenuItems(): Menu[] {
+        //TODO these data will get from database.
+        return [
+            new Menu(1, 'Pizza', 'test store', 4.5, 'https://thumb.photo-ac.com/3e/3ef8c1dab024a40572616f3ec4c3d3b5_w.jpeg'),
+            new Menu(2, 'Burger', 'test store', 4.2, 'https://thumb.photo-ac.com/3e/3ef8c1dab024a40572616f3ec4c3d3b5_w.jpeg'),
+            new Menu(3, 'Pasta', 'test store', 4.1, 'https://thumb.photo-ac.com/3e/3ef8c1dab024a40572616f3ec4c3d3b5_w.jpeg'),
+        ];
+    }
+
+    static getMenuDetail(id: number): Menu | null {
+        const menuItems = MenuService.getMenuItems();
+        const menu = menuItems.find(item => item.id === id);
+        if (menu) {
+            return new MenuDetail(
+                menu.id, 
+                menu.name, 
+                menu.store_name, 
+                menu.review_score, 
+                menu.photo_url, 
+                [], // review_comments
+                []  // listed_history
+            );
+        }
+        return null;
+    }
+}
