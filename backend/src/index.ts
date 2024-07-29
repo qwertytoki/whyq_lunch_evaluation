34aa8a2ab41ca0f4fed8df70072fd9d48f8e8a25
qwertytoki <qwertytoki@gmail.com>
2024-07-29 15:55:26 +0800

    update port to 3001

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -12,7 +12,7 @@ app.get('/health', (req, res) => {
     res.send('Hello World!');
 });
 
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 3001;
 app.listen(port, () => {
     console.log(`Server is running on port ${port}`);
 });
