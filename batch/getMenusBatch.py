e18f36bf5285447b1d9429f26c78b64298106c15
qwertytoki <qwertytoki@gmail.com>
2024-07-31 02:55:08 +0800

    make it headless

--- batch/getMenusBatch.py
+++ batch/getMenusBatch.py
@@ -14,7 +14,7 @@ load_dotenv(dotenv_path="./batch/.env")
 
 def driver_generate():
     chrome_options = Options()
-    # chrome_options.add_argument("--headless")
+    chrome_options.add_argument("--headless")
     chrome_options.add_argument("--start-maximized")
     chrome_options.add_argument("--window-size=1920,1080")
     service = Service(ChromeDriverManager().install())
