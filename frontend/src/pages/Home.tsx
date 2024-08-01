17243e97a8b3cffb3d892560c0e1f976e5647492
qwertytoki <qwertytoki@gmail.com>
2024-08-01 20:11:12 +0800

    use css-module to home

--- frontend/src/pages/Home.tsx
+++ frontend/src/pages/Home.tsx
@@ -1,21 +1,28 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-import '../styles/Home.css';
+
+import styles from '../styles/Home.module.css';
 
 const Home: React.FC = () => {
   return (
-    <div className="home-container">
+    <div className={styles.homeContainer}>
       <h1>WhyQ Lunch Evaluation</h1>
       <nav>
-        <ul>
-          <li>
-            <Link to="/review">Review Page</Link>
+        <ul className={styles.navList}>
+          <li className={styles.navItem}>
+            <Link to="/review" className={styles.navLink}>
+              Review Page
+            </Link>
           </li>
-          <li>
-            <Link to="/menu">Menu Page</Link>
+          <li className={styles.navItem}>
+            <Link to="/menu" className={styles.navLink}>
+              Menu Page
+            </Link>
           </li>
-          <li>
-            <Link to="/ranking">Ranking Page</Link>
+          <li className={styles.navItem}>
+            <Link to="/ranking" className={styles.navLink}>
+              Ranking Page
+            </Link>
           </li>
         </ul>
       </nav>
